a39471aca9e4e547f588c340ec3ed6f17998beed
SonnyP04 <sonnypowell257@gmail.com>
2026-01-02 22:18:17 +0000

    //add routers in budgets

--- backend/app/routers/budgets.py
+++ backend/app/routers/budgets.py
@@ -3,10 +3,21 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Budget
 
-router = APIRouter(prefix=/"budgets", tags=["budgets"])
+router = APIRouter(prefix="/budgets", tags=["budgets"])
 
 @router.get("/")
-def get_budget(db: Session = Depends(get_db)):
+async def get_all_budgets(db: Session = Depends(get_db)):
     budget = db.query(Budget).all()
     return {"budgets": budget}
 
+#@router.post("/budget/")
+#async def create_budget(budget: Budget):
+
+@router.get("/{budget_id}")
+async def get_budget(id: int, budget: Budget, Session = Depends(get_db)):
+    budget = db.query(budget).filter(budget.id == id).first()
+    return budget
+
+
+
+    
