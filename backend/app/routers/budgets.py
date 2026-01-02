1c3da472d5d9f8f9f4b27397b455ba28dba17e59
SonnyP04 <sonnypowell257@gmail.com>
2026-01-02 22:28:54 +0000

    //routers in budgets

--- backend/app/routers/budgets.py
+++ backend/app/routers/budgets.py
@@ -2,20 +2,31 @@ from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Budget
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/budgets", tags=["budgets"])
+
+class CreateBudget(BaseModel):
+    limit : int 
+    category : str 
+    month : str 
 
 @router.get("/")
 async def get_all_budgets(db: Session = Depends(get_db)):
     budget = db.query(Budget).all()
     return {"budgets": budget}
 
-#@router.post("/budget/")
-#async def create_budget(budget: Budget):
+@router.post("/")
+async def create_budget(budget : CreateBudget, db: Session = Depends(get_db)):
+    newBudget = Budget(**budget.dict())
+    db.add(newBudget)
+    db.commit()
+    db.refresh(newBudget)
+    return newBudget
 
 @router.get("/{budget_id}")
-async def get_budget(id: int, budget: Budget, Session = Depends(get_db)):
-    budget = db.query(budget).filter(budget.id == id).first()
+async def get_budget(budget_id: int, db: Session = Depends(get_db)):
+    budget = db.query(Budget).filter(Budget.id == budget_id).first()
     return budget
 
 
